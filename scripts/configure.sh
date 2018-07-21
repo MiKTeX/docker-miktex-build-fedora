65e9c448ad1d61d1adf8d1887a09dcb4dbac3e36
Christian Schenk <cs@miktex.org>
2018-07-21 20:10:08 +0200

    Specify package revision

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+package_revision=${1-1}
 cd /miktex/build
 cmake \
+    -DMIKTEX_PACKAGE_REVISION=${package_revision} \
+    -DMIKTEX_LINUX_DIST=fedora \
+    -DMIKTEX_LINUX_DIST_VERSION=28 \
     -DUSE_SYSTEM_HARFBUZZ=FALSE \
     -DUSE_SYSTEM_HARFBUZZ_ICU=FALSE \
     -DUSE_SYSTEM_POPPLER=FALSE \
